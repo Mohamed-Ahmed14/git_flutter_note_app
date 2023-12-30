be0a252b9d51ac5498064f67f337c7d52bf96edd
Mohamed Ahmed <muhammedahmed14w@gmail.com>
2023-12-30 23:45:51 +0200

    add observer

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app_test/view/note_app/note_app_screen.dart';
 import 'package:note_app_test/view_model/cubit/note_app_cubit/note_app_cubit.dart';
+import 'package:note_app_test/view_model/cubit/observer.dart';
 
 void main()
 {
-
+  Bloc.observer = MyBlocObserver();
 
 
   runApp(
